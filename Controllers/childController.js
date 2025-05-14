db729f464c3be2373f2a980074871989071b62d7
root <root@e2e-113-176.ssdcloudindia.net>
2025-05-14 11:57:56 +0530

    login child

--- Controllers/childController.js
+++ Controllers/childController.js
@@ -103,6 +103,7 @@ class ChildController extends BaseController {
         const token = generateToken(obj);
 
         return res.status(200).json({
+	  success :true ,
           message: "Login successful",
           token,
           data: {
